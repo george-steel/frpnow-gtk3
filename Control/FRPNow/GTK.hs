68e3774fb8a3b09615e716b78b725015fa7b6f8e
George Steel <george.steel@gmail.com>
2017-03-15 00:21:59 -0400

    added function to get events from FileChooserDialogs

--- Control/FRPNow/GTK.hs
+++ Control/FRPNow/GTK.hs
@@ -1,3 +1,5 @@
+
+{-# LANGUAGE LambdaCase, RecursiveDo #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.FRPNow.GTK
@@ -6,14 +8,16 @@
 -- Maintainer  :  atzeus@gmail.org
 -- Stability   :  provisional
 -- Portability :  portable
--- 
+--
 -- This module provides interoperability of FRPNow and the GTK system.
 
 module Control.FRPNow.GTK(
   -- * General interface
-  runNowGTK, setAttr, getSignal, getUnitSignal, getSimpleSignal, getClock,
+  ffor, runNowGTK, setAttr, getSignal, getUnitSignal, getSimpleSignal, getClock,
   -- * Utility functions
-  createLabel, createButton, createProgressBar,createSlider
+  createLabel, createButton, createEntry, createProgressBar,createSlider,
+
+  runFileChooserDialog
   ) where
 
 import Graphics.UI.Gtk
@@ -24,6 +28,11 @@ import Data.IORef
 import Debug.Trace
 import System.Mem.Weak
 import System.Glib.GDateTime
+import qualified Data.Text as T
+import Data.Text (Text)
+
+ffor :: (Functor f) => f a -> (a -> b) -> f b
+ffor = flip fmap
 
 -- | Run a Now computation which can interact with GTK. Also starts the GTK system.
 -- Call only once, or GTK will freak out.
@@ -32,23 +41,23 @@ runNowGTK n = do initGUI
                  doneRef <- newIORef Nothing
                  initNow (schedule doneRef) (n >> return never)
                  mainGUI
-                     
-                 
-                 
-schedule :: IORef (Maybe a) -> IO (Maybe a) -> IO () 
-schedule ref m = postGUIAsync $ 
+
+
+
+schedule :: IORef (Maybe a) -> IO (Maybe a) -> IO ()
+schedule ref m = postGUIAsync $
                    m >>= \x ->
                      case x of
-                      Just _ -> writeIORef ref x 
+                      Just _ -> writeIORef ref x
                       Nothing -> return ()
-                       
+
 -- | Set a GTK attribute to a behavior. Each time the behavior changes the
 -- attribute is updated.
 setAttr :: (WidgetClass w, Eq a) => Attr w a -> w -> Behavior a -> Now ()
-setAttr a w b = 
+setAttr a w b =
      do i <- sample b
         sync $ set w [a := i]
-        (e,cb) <- callback 
+        (e,cb) <- callback
         sync $ on w unrealize ( cb ())
         let updates = toChanges b `beforeEs` e
         callIOStream setEm updates
@@ -56,22 +65,22 @@ setAttr a w b =
 
 
 -- | Obtain an event stream from a unit GTK signal, i.e. a signal with handler type:
--- 
+--
 -- > IO ()
 getUnitSignal :: GObjectClass widget => Signal widget (IO ()) -> widget -> Now (EvStream ())
 getUnitSignal s w = getSignal s w (\f -> f ())
-  
+
 
 -- | Obtain an event stream from a GTK signal giving a single value.
 getSimpleSignal :: GObjectClass widget => Signal widget (value -> IO ()) -> widget -> Now (EvStream value)
-getSimpleSignal s w = getSignal s w id 
+getSimpleSignal s w = getSignal s w id
 
 
--- | General interface to convert an GTK signal to an event stream. 
+-- | General interface to convert an GTK signal to an event stream.
 --
--- The signal has type @callback@, for example @(ScrollType -> Double -> IO Bool)@ 
+-- The signal has type @callback@, for example @(ScrollType -> Double -> IO Bool)@
 -- and the eventstream gives elements of type @value@, for instance @(ScrollType,Double)@
--- The conversion function (3rd argument) takes a function to call for producing the value 
+-- The conversion function (3rd argument) takes a function to call for producing the value
 -- in our example, a function of type @(ScollType,Double) -> IO ()@ and produces
 -- a function of the form @callback@, in our example @(ScrollType -> Double -> IO Bool)@.
 --
@@ -86,7 +95,7 @@ getSimpleSignal s w = getSignal s w id
 --
 -- The signal is automatically disconnected, when the event stream is garbage collected.
 getSignal :: GObjectClass widget => Signal widget callback -> widget -> ((value -> IO ()) -> callback) -> Now (EvStream value)
-getSignal s w conv =    
+getSignal s w conv =
    do (res,f) <- callbackStream
       conn <- sync $ on w s (conv f)
       --sync $ addFinalizer res (putStrLn "Run final" >> signalDisconnect conn)
@@ -95,10 +104,10 @@ getSignal s w conv =
 
 -- | Get a clock that gives the time since the creation of the clock in seconds, and updates maximally even given number of seconds.
 --
--- The clock is automatically destroyed and all resources associated with the clock are freed 
+-- The clock is automatically destroyed and all resources associated with the clock are freed
 -- when the behavior is garbage collected.
 getClock :: Double -> Now (Behavior Double)
-getClock precision = 
+getClock precision =
   do start <- sync $ gGetCurrentTime
      (res,cb) <- callbackStream
      wres<- sync $ mkWeakPtr res Nothing
@@ -106,46 +115,64 @@ getClock precision =
                       let seconds = gTimeValSec now - gTimeValSec start
                       let microsec = gTimeValUSec now - gTimeValUSec start
                       return $ (fromIntegral seconds) + (fromIntegral microsec) * 0.000001
-     let onTimeOut = 
-              deRefWeak wres >>= \x -> 
+     let onTimeOut =
+              deRefWeak wres >>= \x ->
                  case x of
                    Just _ -> getDiff >>= cb >> return True
                    Nothing -> return False
-     sync $ timeoutAdd  onTimeOut (round (precision * 1000)) 
+     sync $ timeoutAdd  onTimeOut (round (precision * 1000))
      sample $ fromChanges 0 res
 
 
 
+runFileChooserDialog :: FileChooserDialog -> Now (Event (Maybe FilePath))
+runFileChooserDialog dialog = do
+    (retev, cb) <- callback
+    sync $ mdo
+        conn <- on dialog response $ \resp -> do
+            widgetHide dialog
+            case resp of
+                ResponseAccept -> do
+                    mfn <- fileChooserGetFilename dialog
+                    cb mfn
+                _ -> cb Nothing
+            signalDisconnect conn
+        widgetShow dialog
+    return retev
+
+
 createLabel :: Behavior String -> Now Label
-createLabel s = 
+createLabel s =
   do l <- sync $ labelNew (Nothing :: Maybe String)
      setAttr labelLabel l s
      return l
 
 
 createButton :: Behavior String ->  Now (Button,EvStream ())
-createButton s =  
-  do button <- sync $ buttonNew 
+createButton s =
+  do button <- sync $ buttonNew
      setAttr buttonLabel button s
      stream <- getUnitSignal buttonActivated  button
      return (button,stream)
 
+createEntry :: String -> Now (Entry, Behavior String)
+createEntry inittext = do
+    entry <- sync $ entryNew
+    sync $ set entry [entryText := inittext]
+    edits <- getSignal editableChanged entry (entryGetText entry >>=)
+    btext <- sample $ fromChanges inittext edits
+    return (entry, btext)
 
-createProgressBar :: Now (ProgressBar, Double -> IO ()) 
-createProgressBar =
-              do (evs, cb) <- callbackStream
-                 progress <- sample $ fromChanges 0 evs
-                 bar <- sync $ progressBarNew
-                 setAttr progressBarFraction bar progress
-                 return (bar,cb)
+createProgressBar :: Behavior Double -> Now ProgressBar
+createProgressBar progress = do
+    bar <- sync $ progressBarNew
+    setAttr progressBarFraction bar progress
+    return bar
 
 createSlider ::  Double -> Double -> Double -> Behavior Double -> Now (HScale,EvStream Double)
-createSlider min max step b =  
+createSlider min max step b =
   do i <- sample b
      slider <- sync $ hScaleNewWithRange min max step
      setAttr rangeValue slider b
-     stream <- getSignal changeValue slider (\f _ d -> f d >> return True) 
+     stream <- getSignal changeValue slider (\f _ d -> f d >> return True)
      return (slider,stream)
-
-
-
